52808778eb50c9c2ef6dc16bbde995f2ff2b3162
abedSamer <eng.abed.s.sh@gmail.com>
2021-08-28 21:03:33 +0300

    .

--- header.js
+++ header.js
@@ -2,11 +2,9 @@ window.addEventListener("scroll", () => {
   const scrolled = window.scrollY;
   var test = document.getElementsByClassName("section_discription");
   var mainMenu = document.getElementById("header");
-  if (scrolled > 104) {
-    mainMenu.style.position = "sticky";
-    mainMenu.style.top = " -106px ";
+  if (scrolled != 0) {
+    document.documentElement.style.setProperty("--headerHeight", "80px");
   } else {
-    mainMenu.style.position = "";
-    mainMenu.style.top = "";
+    document.documentElement.style.setProperty("--headerHeight", "100px");
   }
 });
