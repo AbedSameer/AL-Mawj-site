ec7f9d6c709ba6525a84e424f72b82093c0191e8
abedSamer <eng.abed.s.sh@gmail.com>
2021-08-31 17:00:27 +0300

    responsev

--- header.js
+++ header.js
@@ -1,10 +1,16 @@
 window.addEventListener("scroll", () => {
-  const scrolled = window.scrollY;
+  const scrolled = window.scrollY,
+    secreenSize = window.screen.availWidth;
   var test = document.getElementsByClassName("section_discription");
   var mainMenu = document.getElementById("header");
-  if (scrolled != 0) {
+  if (scrolled != 0 && secreenSize > 768) {
     document.documentElement.style.setProperty("--headerHeight", "80px");
+  } else if (scrolled != 0 && secreenSize <= 768) {
+    document.documentElement.style.setProperty("--headerHeight", "60px");
   } else {
-    document.documentElement.style.setProperty("--headerHeight", "100px");
+    if (secreenSize > 768)
+      document.documentElement.style.setProperty("--headerHeight", "100px");
+    if (secreenSize <= 768)
+      document.documentElement.style.setProperty("--headerHeight", "80px");
   }
 });
